adc265ef13203cb0c19bfcf7b4b3a7b8d43397b5
Sergiy Kononenko <serkononenko@gmail.com>
2019-09-26 15:42:39 +0300

    added startStop handler

--- src/components/TimerControl.js
+++ src/components/TimerControl.js
@@ -4,11 +4,15 @@ import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import CachedIcon from '@material-ui/icons/Cached';
 
-export const TimerControl = ({ isLaunched, start, pause, reset }) => {
+export const TimerControl = ({ isLaunched, startStop, reset }) => {
+    const handleClick = () => {
+        startStop(isLaunched);
+    }
+
     return (
         <Grid container justify='space-around'>
             <Grid item>
-                <IconButton id='start_stop' color='inherit' onClick={isLaunched ? pause : start}>
+                <IconButton id='start_stop' color='inherit' onClick={handleClick}>
                     {isLaunched ? <PauseCircleOutlineIcon fontSize='large' /> : <PlayCircleOutlineIcon fontSize='large' />}
                 </IconButton>
             </Grid>
